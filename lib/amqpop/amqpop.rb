6da8e2ec2f0f81e78d1978c5e38dc6496dbd7a99
Chris Cherry <ctcherry@gmail.com>
2014-10-29 18:24:17 -0700

    whitespace cleanup

--- lib/amqpop/amqpop.rb
+++ lib/amqpop/amqpop.rb
@@ -1,21 +1,21 @@
 module Amqpop
 
-	class << self
-		attr_accessor :options
+  class << self
+    attr_accessor :options
     attr_accessor :connection
-	end
+  end
 
-	self.options = nil
+  self.options = nil
 
-	def self.require_ack?
+  def self.require_ack?
     return nil if options.nil?
-	  !temp_queue?
-	end
+    !temp_queue?
+  end
 
-	def self.temp_queue?
-		return nil if options.nil?
-	  options[:queue_name] == ""
-	end
+  def self.temp_queue?
+    return nil if options.nil?
+    options[:queue_name] == ""
+  end
 
   def self.eputs(msg)
     STDERR.puts msg
